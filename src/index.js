700a920371bea05baf7de495dc1a677a75978c75
oqtoshqoratoshgapirmaganqozichoq <epaxaboz@britishpreschool.net>
2022-06-15 10:18:08 +0500

    about and contact deleted

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,8 +12,6 @@ root.render(
       <Routes>
         <Route path="/" element={<App/>}>
           <Route index element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
