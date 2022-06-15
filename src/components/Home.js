b4b148e4d676932a3fe21577c5fb2d60e15fb1b9
Ubaydullayev__12 <ubaydullayevnurullo4@gmail.com>
2022-06-15 10:40:54 +0500

    home ozgardi botedn1

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,10 @@ export default function Home() {
     return (
         <div className="App">
             Home
+            <p>loremhsdfkjasdhfkjsadhkfjsadfsad</p>
+            <p>loremhsdfkjasdhfkjsadhkfjsadfs sjflksadfshdf</p>
+            <p>loremhsdfkjasdhfkjsadhkfjsadfs sjflksadfshdf sdl;fjsfajsdf;sadjfs</p>
+            <p>loremhsdfkjasdhfkjsadhkfjsadfs sjflksadfshdf sdl;fjsfajsdf;sadjfs HSFKSFSAFSD </p>
         </div>
     )
 }
